8c6cb169ccdc8e300c8a47fc0058b3083471e914
Rawin Deeboonchai <comtalyst@outlook.com>
2019-04-04 20:40:06 -0400

    Efficient wall following for straight line
    Originally Updated 3/31/2019
    Version name: awesome
    
    This version has been demonstrated to Chris on the outside wall of the lab room.

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,7 @@
-// Firex
+/*
+ * Maze traversing module
+ * 
+ */
 
 #define MAX 9999
 
@@ -7,11 +10,15 @@ const int LEDPin = 13;
 char side = 'R';            // focusing side of wall following
 
 const float minFurther = 1;        // minimum distance difference between two sensors that would make the robot readjust itself
-const float minFar = 20;            // minimum distance from sensor that would be considered far
+const float minFar = 40;            // minimum distance from sensor that would be considered far
 const float minWalkable = 0;       // minimum distance needed in front of the bot for it to walk forward
+const int minTickToBeStraight = 6;
 
 int tick;
 int lastBalance;
+int lastSense = 15;
+int lastFar = -MAX;
+int lastLineDetect = -MAX;
 
 void setup() {
   Serial.begin(9600);                     // Open the serial port
@@ -24,59 +31,91 @@ void setup() {
 }
 
 void loop() {
-  if(side == 'R'){      // TOMORROW --> MIMIC TO THE LEFT
+  /*if(side == 'R'){
     float rangeRightFront = getRangeRightFront();
     float rangeRightRear = getRangeRightRear();
+    if(tick-lastFar >= minTickToBeStraight && !isFar(rangeRightFront)){
+      lastSense = rangeRightFront;
+    }
+    if(tick - lastLineDetect > 20 && detectLine()){
+      lastLineDetect = tick;
+      alignBot();
+      forwardFast(10);
+      robotStop(20);
+      // search for candle
+      right90(2);
+    }
     // IF SIDE-FRONT SENSOR IS FAR OR SIDE-FRONT SENSOR IS SIGNIFICANTLY FURTHER THAN SIDE-REAR SENSOR
-    if(isFar(rangeRightFront)){
+    else if(isFar(rangeRightFront)){
       digitalWrite(LEDPin, HIGH);
-      rightSlightly();
+      rightSlightly(1);
+      forwardSlightly((int)(lastSense*(4.0/13.0)));       // steps : wall vs sensor (the more the broader)
       digitalWrite(LEDPin, LOW);
       lastBalance = tick;
+      lastFar = tick;
     }
     else if(rangeRightFront - (isFar(rangeRightRear)? MAX:rangeRightRear) >= minFurther){
       // TURN RIGHT SLIGHTLY
-      rightSlightly();
-      rightSlightly();
-      for(int i = 0; i < tick-lastBalance; i++){
-        forwardSlightly();
+      if(lastBalance == tick-1){
+        rightSlightly(1);
       }
-      rightSlightly();
+      else{
+        rightSlightly(2);
+        for(int i = 0; i < tick-lastBalance; i++){
+          forwardSlightly(1);
+          if(isFar(getRangeRightFront())){
+            break;
+          }
+        }
+        leftSlightly(1);
+      }
       lastBalance = tick;
     }
     // IF SIDE-REAR SENSOR IS SIGNIFICANTLY FURTHER THAN SIDE-FRONT SENSOR
     else if(rangeRightRear - rangeRightFront >= minFurther){
       // TURN LEFT SLIGHTLY
-      leftSlightly();
+      if(lastBalance == tick-1){
+        leftSlightly(1);
+      }
+      else{
+        leftSlightly(2);
+        for(int i = 0; i < tick-lastBalance; i++){
+          forwardSlightly(1);
+          if(isFar(getRangeRightFront())){
+            break;
+          }
+        }
+        rightSlightly(1);
+      }
       lastBalance = tick;
     }
     else if(getRangeFrontLow() >= minWalkable){
       // FORWARD SLIGHTLY
-      forwardSlightly();
+      forwardSlightly(1);
     }
     else{
       // TURN RIGHT 90
-      right90();
+      right90(1);
     }
   }
   else if(side == 'L'){
     // IF SIDE-FRONT SENSOR IS FAR OR SIDE-FRONT SENSOR IS SIGNIFICANTLY FURTHER THAN SIDE-REAR SENSOR
     if(isFar(getRangeLeftFront()) || getRangeLeftFront() - getRangeLeftRear() >= minFurther){
       // TURN LEFT SLIGHTLY
-      leftSlightly();
+      leftSlightly(1);
     }
     // IF SIDE-REAR SENSOR IS SIGNIFICANTLY FURTHER THAN SIDE-FRONT SENSOR
     else if(getRangeLeftRear() - getRangeLeftFront() >= minFurther){
       // TURN RIGHT SLIGHTLY
-      rightSlightly();
+      rightSlightly(1);
     }
     else if(getRangeFrontLow() >= minWalkable){
       // FORWARD SLIGHTLY
-      forwardSlightly();
+      forwardSlightly(1);
     }
     else{
       // TURN LEFT 90
-      left90();
+      left90(1);
     }
   }
   else{ // something's wrong
@@ -84,5 +123,5 @@ void loop() {
     Serial.println(side);
     while(true);
   }
-  tick++;
+  tick++;*/
 }
